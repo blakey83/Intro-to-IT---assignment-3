9bb7fabdd96ece83aa9164a051c1ccb53297277b
drinkwatern <80311348+drinkwatern@users.noreply.github.com>
2021-04-11 08:41:45 +1000

    Most recent website files

---
+++ Website/scripts.js
@@ -0,0 +1,110 @@
+$(document).ready(function () {
+       $("#getInfo").click(function () {
+           $('#Details').on('show.bs.modal');
+           $(".modal-title").text("Nicholas Drinkwater");
+           $(".mainText").text("Hey! I’m Nick Drinkwater (s3508178) of UMI. I was born in Sydney but grew up in Melbourne. Whilst I don’t have any formal IT experience, I actually previously started this degree back in 2015. After completing half a year, I left to go traveling, spending 2 years living in Edinburgh, Scotland. With Coronavirus and being unable to travel, I decided to use this time to return and finish my degree! I love traveling, film, and baseball. My interests in IT include programming, building IT systems, and hardware. I hope to learn the necessary skills and knowledge to help me enter the IT industry in my career, as well as exposure to inspiring topics and fields of IT that I haven’t seen or encountered before.");
+           $(".mainTextBanner1").text("Myers-Briggs")
+           $(".mainText1").text("My Myers-Briggs test indicated that I am an INFP-T, which is a Turbulent Mediator. This means that I am thoughtful, open-minded and empathetic but can also overly self-critical and idealistic.");
+           $(".mainTextBanner2").text("Honey and Mumford")
+           $(".mainText2").text("My Honey and Mumford score indicated that I have a Reflector learning style, which is someone who learns by observing and considering what happened and someone who likes to consider all the possibilities and implications.");
+           $(".mainTextBanner3").text("Big 5 Personality Test");
+           $(".mainText3").text("My third test was the Big 5 Personality test that indicated that I am primarily good natured, courteous and supportive but I can also be shy and tend to worry about things.");
+           $(".mainTextBanner4").text("Discussion");
+           $(".mainText4").text("I believe that these test results put me in a really good position for working within a team! My strengths will mesh really well within a team as I'm always respectful and open to any ideas that my teammates may have! My results show I’m passionate, generous and dedicated which I will use to ensure that I fit in and work well within the team.");
+           })
+       });
+
+$(document).ready(function () {
+        $("#getInfo1").click(function () {
+          $('#Details').on('show.bs.modal');
+          $(".modal-title").text("Erin Paton");
+          $(".mainText").text("I’m Erin (s3910930) of UMI. Originally from Sydney, I relocated to Byron Bay for a change of pace and to get closer to nature—most of my weekends are now spent hiking, exploring waterfalls, or at the beach. Currently, I’m fluent in English but I have a passion for French culture and intend to study the language up to C-level (only after I’ve knocked over a few programming languages!). My experience with IT is specific to marketing technology, and the software used to implement it. Art, Design, Psychology, Health & Wellness, and Sexual Education are all areas that are important to me and while I don’t have plans to further my education in these fields, the possibility of exploring their convergence with emerging technologies is something that excites me. I’d love to play a role in increasing the accessibility and interoperability of these spaces in some way throughout the course of my career.");
+          $(".mainTextBanner1").text("Myers Briggs (INFP)");
+          $(".mainText1").text("My results indicate introversion, an intuitive information-gathering style, emotion-based decision making, and a preference for structure and organisation in my environment. Dubbed “The Advocate”, 16 Personalities describes individuals of this combination as creative problem-solvers with the ability to accurately read people’s true feelings through compassion and keen intuition. Advocates are inspiring communicators who speak and write with a conviction that can be quite persuasive. They prefer to succeed to the benefit of those around them, never to the detriment of others. Some weaknesses of the Advocate profile are a sensitivity to criticism, difficulty opening up and asking for help, and a sometimes debilitating perfectionism.");
+          $(".mainTextBanner2").text("VARK (AK Type Two)");
+          $(".mainText2").text("My results show a multimodal inclination towards auditory and kinesthetic learning styles. Auditory learners digest information that is heard or spoken with a preference for group discussions, lectures, talking things over, as well as some informal writing methods that are formatted colloquially. Kinesthetic learners value implementation that is connected to reality, favouring demonstrations and simulations. VARK Type Two individuals work best when they’ve gathered information from both of their preferred learning modalities and often take more time exploring subject matter which can appear as procrastination or inefficiency but generally leads to a more in-depth comprehension of the material.");
+          $(".mainTextBanner3").text("DISC (Type S)");
+          $(".mainText3").text("My results indicate a strong egalitarian inclination and a desire to cultivate a supportive, inclusive environment for teammates to flourish in. Type S individuals are dependable, reliable, strong communicators who utilise compassion and diplomacy in adversity and prefer to diffuse conflict with patience and understanding. They’re natural collaborators and strive for the overall success of the team rather than advancing their individual endeavours. These types can be indecisive and over-accommodating, often to their own and the team’s detriment, which can ultimately have the opposite effect of their intentions if not kept in check. Being extremely empathetic people, they often gloss over problems and avoid calling out the inadequacies of their colleagues in an attempt to keep the peace, leading to setbacks down the line.");
+          $(".mainTextBanner4").text("");
+          $(".mainText4").text("");
+          })
+      });
+
+$(document).ready(function () {
+        $("#getInfo2").click(function () {
+          $('#Details').on('show.bs.modal');
+          $(".modal-title").text("NAME");
+          $(".mainText").text("");
+          $(".mainTextBanner1").text("");
+          $(".mainText1").text("");
+          $(".mainTextBanner2").text("");
+          $(".mainText2").text("");
+          $(".mainTextBanner3").text("");
+          $(".mainText3").text("");
+          $(".mainTextBanner4").text("");
+          $(".mainText4").text("");
+          })
+      });
+$(document).ready(function () {
+        $("#getInfo3").click(function () {
+          $('#Details').on('show.bs.modal');
+          $(".modal-title").text("NAME");
+          $(".mainText").text("");
+          $(".mainTextBanner1").text("");
+          $(".mainText1").text("");
+          $(".mainTextBanner2").text("");
+          $(".mainText2").text("");
+          $(".mainTextBanner3").text("");
+          $(".mainText3").text("");
+          $(".mainTextBanner4").text("");
+          $(".mainText4").text("");
+          })
+      });
+
+$(document).ready(function () {
+        $("#getInfo4").click(function () {
+          $('#Details').on('show.bs.modal');
+          $(".modal-title").text("NAME");
+          $(".mainText").text("");
+          $(".mainTextBanner1").text("");
+          $(".mainText1").text("");
+          $(".mainTextBanner2").text("");
+          $(".mainText2").text("");
+          $(".mainTextBanner3").text("");
+          $(".mainText3").text("");
+          $(".mainTextBanner4").text("");
+          $(".mainText4").text("");
+          })
+      });
+
+$(document).ready(function () {
+        $("#getInfo5").click(function () {
+          $('#Details').on('show.bs.modal');
+          $(".modal-title").text("NAME");
+          $(".mainText").text("");
+          $(".mainTextBanner1").text("");
+          $(".mainText1").text("");
+          $(".mainTextBanner2").text("");
+          $(".mainText2").text("");
+          $(".mainTextBanner3").text("");
+          $(".mainText3").text("");
+          $(".mainTextBanner4").text("");
+          $(".mainText4").text("");
+          })
+      });
+
+$(document).ready(function () {
+        $("#getInfo6").click(function () {
+          $('#Details').on('show.bs.modal');
+          $(".modal-title").text("NAME");
+          $(".mainText").text("");
+          $(".mainTextBanner1").text("");
+          $(".mainText1").text("");
+          $(".mainTextBanner2").text("");
+          $(".mainText2").text("");
+          $(".mainTextBanner3").text("");
+          $(".mainText3").text("");
+          $(".mainTextBanner4").text("");
+          $(".mainText4").text("");
+          })
+            });
